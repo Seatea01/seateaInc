21f621f9649b44909c01a286b91925ac9569e43e
Seatea01 <51541607+Seatea01@users.noreply.github.com>
2021-02-22 16:05:05 +0100

    Update signup.php
    Css

--- signup.php
+++ signup.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>MyChat | Sign Up</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style type="text/css">
 		@font-face{
 			font-family: headFont;
