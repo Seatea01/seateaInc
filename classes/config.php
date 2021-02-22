ca471f2d58e07e9b5860e4f8cd038aba9878c2ca
Seatea01 <51541607+Seatea01@users.noreply.github.com>
2021-02-22 12:23:17 +0100

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DBNAME', "tosin");
+define('DBNAME', "heroku_2f65c3d32b5755d");
 define('DBUSER', "bdd4e62a44aca9");
 define('DBPASS', "ebd092d5");
 define('DBHOST', "us-cdbr-east-03.cleardb.com");
