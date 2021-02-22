c3cf376fb49cb528f0b517fe67566e221b5820ff
Seatea01 <51541607+Seatea01@users.noreply.github.com>
2021-02-22 09:26:00 +0100

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DBNAME', "i63ky60fkiv1kh2w");
+define('DB', "i63ky60fkiv1kh2w");
 define('DBUSER', "tx0xztmm7ucp2bjg");
 define('DBPASS', "jq2prsigpg3k2f0x");
 define('DBHOST', "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
