df39f0acd06a9a90576eef0a53d152c92bea7ce1
Seatea01 <51541607+Seatea01@users.noreply.github.com>
2021-02-22 17:12:56 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>MyChat | Sign Up</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<title>SeateaInc | Sign Up</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<style type="text/css">
 		@font-face{
 			font-family: headFont;
@@ -66,14 +66,14 @@
 			display: none;
 		}
                 
-@media only screen and (max-width: 600px) {
-  #wrapper {
-  }
-form{
-padding: 20px;
-margin: auto;
-}
-}
+		@media only screen and (max-width: 600px) {
+		  #wrapper {
+		  }
+			form{
+			padding: 20px;
+			margin: auto;
+			}
+		}
 </style>
 </head>
 <body>
