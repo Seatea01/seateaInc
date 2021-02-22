5f932ad78ba2e1abf6ef5911acce53a18c1327a0
Seatea01 <51541607+Seatea01@users.noreply.github.com>
2021-02-22 16:53:33 +0100

    Update login.php
    Css

--- login.php
+++ login.php
@@ -3,7 +3,11 @@
 <html>
 <head>
 	<title>MyChat | Sign Up</title>
-	<style type="text/css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	<style type="text/css">
 		@font-face{
 			font-family: headFont;
 			src: url(ui/fonts/Summer-Vibes-OTF.otf);
@@ -38,11 +42,10 @@
 			color: black;
 		}
 		input[type=text], input[type=password], input[type=button]{
-			width: 100%;
 			color: gray;
 			border-radius: 5px;
 			border: solid thin gray;
-			height: 25px;
+			
 		}
 		input[type=button]{
 			 background-color: #1b6879;
@@ -69,10 +72,10 @@
 		<div id="header">myChat</div>
 		<div id="error"></div>
 		<form id="myform">
-			<input type="text" name="email" id="email" placeholder="Email"><br><br>
-			<input type="password" name="password" id="password" placeholder="Password"><br><br>
+			<input class="form-control form-control-sm" type="text" name="email" id="email" placeholder="Email"><br>
+			<input class="form-control form-control-sm" type="password" name="password" id="password" placeholder="Password"><br>
 			
-			<input type="button" id="login_button" value="Log in"><br><br>
+			<input class="btn btn-sm" type="button" id="login_button" value="Log in"><br><br>
 			<div style="display: block; text-align: center;">Don't have an account? Kindly <a style="text-decoration: none;" href="signup.php"> Signup </a> here</div>
 		
 			<br>
